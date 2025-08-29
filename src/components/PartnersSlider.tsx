b695923617bec8527a68c6639ad562b23aa98569
boinimahesh <mahesh@mahasos.com>
2025-08-29 20:37:19 +0530

    fixes slider

--- src/components/PartnersSlider.tsx
+++ src/components/PartnersSlider.tsx
@@ -1,59 +1,69 @@
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Autoplay } from 'swiper/modules';
 import { TypeAnimation } from 'react-type-animation';
 import JulyLogo from "/6july-logo.svg?url";
 import BPOLogo from "/bpo-logo.svg?url";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { useRef, useState } from "react";
 import 'swiper/css';
+import 'swiper/css/effect-fade';
 import useResponsive from '../hooks/responsive.hook';
 
 const slides = [
     {
-      image: JulyLogo,
-      text: "Design agency, Hyderabad"
+        image: JulyLogo,
+        textSequence: [
+            'Design agency, Hyderabad',
+            1000,
+        ],
     },
     {
-      image: BPOLogo,
-      text: "BPO Solutions Group, Tijuana"
+        image: BPOLogo,
+        textSequence: [
+            'BPO Solutions Group, Tijuana',
+            1000,
+        ],
     },
-  ];
+];
 
 const PartnersSlider = () => {
-    const swiperRef = useRef<any>(null);
-      const [activeIndex, setActiveIndex] = useState<number>(0);
-      const { isMobile } = useResponsive();
+    const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+    const { isMobile } = useResponsive();
     return (
         <div className="partners-slider-items">
             <Swiper
                 direction={isMobile ? "horizontal" : "vertical"}
-                spaceBetween={0}
+                modules={[EffectFade, Autoplay]}
+                effect="fade"
+                fadeEffect={{ crossFade: true }}
+                spaceBetween={30}
                 slidesPerView={1}
-                onSwiper={(swiper) => (swiperRef.current = swiper)}
-                onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+                loop={true}
+                autoplay={{
+                    disableOnInteraction: false,
+                }}
+                onSlideChange={(swiper) => {
+                setActiveSlideIndex(swiper.realIndex);
+                }}
             >
                 {slides.map((slide, index) => (
-                    <SwiperSlide key={index}>
+                    <SwiperSlide 
+                        key={index}
+                    >
                         <div className="flex flex-col md:flex-row items-start md:items-center gap-6 w-full h-full">
                             <div className="flex items-center">
-                                <img src={slide.image} alt="brand" />
+                                <img src={slide.image} alt={`slide ${index + 1}`} />
                             </div>
                             <div className="flex flex-1">
-                                {activeIndex === index ? (
+                                {index === activeSlideIndex && (
                                     <TypeAnimation
-                                        key={`${index}-${activeIndex}`}
-                                        sequence={[
-                                            `${slide.text}`,
-                                            2000,
-                                            () => {
-                                                swiperRef.current?.slideNext();
-                                            },
-                                        ]}
+                                        sequence={slide.textSequence}
                                         wrapper="span"
                                         speed={50}
                                         className=" font-normal font-body text-2xl md:text-4xl leading-full"
-                                        // repeat={Infinity}
+                                        repeat={Infinity}
                                         cursor={true}
                                     />
-                                ) : null}
+                                )}
                             </div>
                         </div>
                     </SwiperSlide>
